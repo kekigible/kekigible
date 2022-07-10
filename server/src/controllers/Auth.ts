45a918e3dfcca616fe25cb0949971002edcc78d3
Rispectech <ue208085.rishabhverma.it@gmail.com>
2022-07-10 18:38:46 +0530

    multiple handlers

--- server/src/controllers/Auth.ts
+++ server/src/controllers/Auth.ts
@@ -10,52 +10,8 @@ import {
   verifyRefreshToken,
 } from "../utils/Utils";
 import { PayloadType } from "../types";
-
-const registerUser = AsyncWrapper(async (req, res) => {
-  const { username, password, email } = req.body;
-  let hashedPassword = bcrypt.hashSync(password, 8);
-
-  if (!username || !password) throw new BadRequest("Please enter username and password");
-
-  let user = await User.findOne({ email: req.body.email });
-  if (user) throw new BadRequest("User already present");
-
-  user = await User.create({
-    username: username,
-    email: email,
-    password: hashedPassword,
-  });
-  // create a token
-  // var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
-  //   expiresIn: 86400, // expires in 24 hours
-  // });
-
-  const token = createAccessToken(user._id);
-  sendRefreshTokenCookie(res, createRefreshToken(user.id));
-  res.status(200).json({ auth: true, token: token });
-});
-
-const loginUser = AsyncWrapper(async (req, res) => {
-  // console.log("req user", req.id);
-
-  const user = await User.findOne({ id: req.id });
-  if (!user) throw new ErrorClass(400, "No such user present");
-
-  var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-
-  if (!passwordIsValid)
-    return res
-      .status(401)
-      .json({ auth: false, token: null, message: "Password is incorrect" });
-
-  // var token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
-  //   expiresIn: "5min", // expires in 24 hours
-  // });
-
-  const token = createAccessToken(user._id);
-  res.status(200).json({ status: "success , loged in", data: user, token: token });
-  //   if (!username || !password) throw new ErrorClass(400, "Please enter username and password");
-});
+import Company from "../entities/Company";
+import Admin from "../entities/Admin";
 
 const refreshToken = async (req: Request, res: Response, next: NextFunction) => {
   // eslint-disable-next-line no-underscore-dangle
@@ -80,4 +36,123 @@ const refreshToken = async (req: Request, res: Response, next: NextFunction) => 
   }
 };
 
-export { registerUser, loginUser, refreshToken };
+//USER login/register
+
+const registerUser = AsyncWrapper(async (req, res) => {
+  const { username, password, email } = req.body;
+  let hashedPassword = bcrypt.hashSync(password, 8);
+
+  if (!username || !password) throw new BadRequest("Please enter username and password");
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) throw new BadRequest("User already present");
+
+  user = await User.create({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+
+  const token = createAccessToken(user._id);
+  sendRefreshTokenCookie(res, createRefreshToken(user.id));
+  res.status(200).json({ auth: true, token: token });
+});
+
+const loginUser = AsyncWrapper(async (req, res) => {
+  const user = await User.findOne({ id: req.id });
+  if (!user) throw new ErrorClass(400, "No such user present");
+
+  var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+  if (!passwordIsValid)
+    return res
+      .status(401)
+      .json({ auth: false, token: null, message: "Password is incorrect" });
+
+  const token = createAccessToken(user._id);
+  res.status(200).json({ status: "success , loged in", data: user, token: token });
+});
+
+//Company
+
+const registerCompany = AsyncWrapper(async (req, res) => {
+  const { username, password, email } = req.body;
+  let hashedPassword = bcrypt.hashSync(password, 8);
+
+  if (!username || !password) throw new BadRequest("Please enter username and password");
+
+  let user = await Company.findOne({ email: req.body.email });
+  if (user) throw new BadRequest("User already present");
+
+  user = await User.create({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+
+  const token = createAccessToken(user._id);
+  sendRefreshTokenCookie(res, createRefreshToken(user.id));
+  res.status(200).json({ auth: true, token: token });
+});
+
+const loginCompany = AsyncWrapper(async (req, res) => {
+  const user = await Company.findOne({ id: req.id });
+  if (!user) throw new ErrorClass(400, "No such user present");
+
+  var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+  if (!passwordIsValid)
+    return res
+      .status(401)
+      .json({ auth: false, token: null, message: "Password is incorrect" });
+
+  const token = createAccessToken(user._id);
+  res.status(200).json({ status: "success , loged in", data: user, token: token });
+});
+
+//ADMIN
+
+const registerAdmin = AsyncWrapper(async (req, res) => {
+  const { username, password, email } = req.body;
+  let hashedPassword = bcrypt.hashSync(password, 8);
+
+  if (!username || !password) throw new BadRequest("Please enter username and password");
+
+  let user = await Admin.findOne({ email: req.body.email });
+  if (user) throw new BadRequest("User already present");
+
+  user = await User.create({
+    username: username,
+    email: email,
+    password: hashedPassword,
+  });
+
+  const token = createAccessToken(user._id);
+  sendRefreshTokenCookie(res, createRefreshToken(user.id));
+  res.status(200).json({ auth: true, token: token });
+});
+
+const loginAdmin = AsyncWrapper(async (req, res) => {
+  const user = await Admin.findOne({ id: req.id });
+  if (!user) throw new ErrorClass(400, "No such user present");
+
+  var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+
+  if (!passwordIsValid)
+    return res
+      .status(401)
+      .json({ auth: false, token: null, message: "Password is incorrect" });
+
+  const token = createAccessToken(user._id);
+  res.status(200).json({ status: "success , loged in", data: user, token: token });
+});
+
+export {
+  registerUser,
+  loginUser,
+  refreshToken,
+  loginCompany,
+  loginAdmin,
+  registerCompany,
+  registerAdmin,
+};
--- server/src/routes/Auth.ts
+++ server/src/routes/Auth.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
-import { loginUser, refreshToken, registerUser } from "../controllers/Auth";
+import {
+  loginUser,
+  refreshToken,
+  registerUser,
+  loginCompany,
+  registerCompany,
+  registerAdmin,
+  loginAdmin,
+} from "../controllers/Auth";
 import authMiddleware from "../middleware/Auth";
 
 const AuthRouter = Router();
@@ -8,4 +16,11 @@ AuthRouter.route("/refreshToken").get(refreshToken);
 AuthRouter.route("/auth/login/user").post(authMiddleware, loginUser);
 AuthRouter.route("/auth/register/user").post(registerUser);
 
+//company
+AuthRouter.route("/auth/login/company").post(authMiddleware, loginCompany);
+AuthRouter.route("/auth/register/cpmpany").post(registerCompany);
+
+//admin
+AuthRouter.route("/auth/login/admin").post(authMiddleware, loginAdmin);
+AuthRouter.route("/auth/register/admin").post(registerAdmin);
 export default AuthRouter;

2f3fc4c6e613515000895812b9b4e652ea1ee3fe
rik61072@gmail.com <rik61072@gmail.com>
2022-07-13 22:05:11 +0530

    compilable yet untested contract with OpenZeppelin

--- contracts/contracts/Kekigible.sol
+++ contracts/contracts/Kekigible.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 //import "@openzeppelin/contracts/access/Roles.sol";
 
-contract Kekigible is ERC1155("localhost:8000/product/{id}.json") {
+contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl {
     //using Roles for Roles.Role;
 
     // TODO:
@@ -18,20 +19,24 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json") {
     //   - Details of times when warranty was availed (should be in backend ig???)
     //     - Backend should be checking blockchain as source of truth so ig so
     // - public functions to external, when done
-    // - msg.sender to _msgSender()
+    // - msg.sender to _msg.sender
     
     // apparently solidity works better with 256, using smaller datatype leads to more cost
     uint256 public constant LoyaltyToken = 0; //ID of loyalty token, only token that is non-fungible
-    mapping (address => bool) public Admins;
-    mapping (address => bool) public CertifiedMinters;
-    mapping (address => bool) public BannedAccounts; // could be both company/user
+    // mapping (address => bool) public Admins;
+    // mapping (address => bool) public CertifiedMinters;
+    // mapping (address => bool) public BannedAccounts; // could be both company/user
     mapping (uint256 => NFTMetadata) public Warranties; // ID -> NFTMetadata
 
     //Roles.Role private _minters;
     //Roles.Role private _bannedAccounts;
 
+    bytes32 public constant MINTER = keccak256("MINTER");
+    bytes32 public constant BANNED = keccak256("BANNED");
+
     struct NFTMetadata {
         int times; //negative means no longer valid
+        bool decay; //should NFT be also invalid if warranty is decayed?
         string productName;
         string productDescription;
         uint timestamp; //timestamp till which warranty valid
@@ -42,54 +47,36 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json") {
 
 
     constructor(){
-        Admins[msg.sender] = true;
-        CertifiedMinters[msg.sender] = true;
+        // Admins[msg.sender] = true;
+        // CertifiedMinters[msg.sender] = true;
+
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER, msg.sender);
+        _setRoleAdmin(BANNED, MINTER); //minters can ban people
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 
     /////////////////////////////////Modifiers////////////////////////////////
 
     modifier onlyAdmin{
-        require(Admins[msg.sender], "This is Admin Only!");
+        // require(Admins[msg.sender], "This is Admin Only!");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "This is Admin Only!");
         _;
     }
 
     modifier onlyNonBanned{
-        require(!(BannedAccounts[msg.sender]), "You have been banned, contact admin!");
+        require(!(hasRole(BANNED, msg.sender)), "You have been banned, contact admin!");
         _;
     }
 
-    modifier onlyCertifiedMinters{
-        require(CertifiedMinters[msg.sender], "You are not certified to mint!");
+    modifier onlyMinters{
+        require(hasRole(MINTER, msg.sender), "You are not certified to mint!");
         _;
     }
 
-    ///////////////////////////////Utility Functions//////////////////////////
-
-    function isAdmin(address _query) public view returns(bool) {
-        return Admins[_query];
-    }
-
-    function isBanned(address _query) public view returns(bool) {
-        return BannedAccounts[_query];
-    }
-
-    function isCertifiedMinter(address _query) public view returns(bool) {
-        return CertifiedMinters[_query];
-    }
-
-    function addAdmin(address newAdmin) onlyAdmin public {
-        Admins[newAdmin] = true;
-    }
-
-    function addCertifiedMinter(address newMinter) onlyAdmin public {
-        CertifiedMinters[newMinter] = true;
-    }
-
-    function ban(address someDude) onlyAdmin public {
-        BannedAccounts[someDude] = true;
-    }
-
-    ///////////////////////////////extension of ERC-1199 functions//////////////////
 
     function mint(
         address account,
@@ -97,15 +84,18 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json") {
         uint256 amount,
         bytes memory data,
         int times,
+        bool decay,
         string memory productName,
         string memory productDescription,
         uint timestamp,
         bool voidWhenSold,
         bool soulBound,
         bool applied
-        ) public onlyCertifiedMinters {
+        ) public onlyMinters {
+            require(!(hasRole(BANNED, account)), "Address is banned!");
             Warranties[id] = NFTMetadata(
                 times,
+                decay,
                 productName,
                 productDescription,
                 timestamp,

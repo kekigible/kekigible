48d60a32fe24d47a8bbb290080a45cf2bd3219dd
rik61072@gmail.com <rik61072@gmail.com>
2022-07-15 02:20:44 +0530

    made  minting friendly

--- contracts/contracts/Kekigible.sol
+++ contracts/contracts/Kekigible.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
 contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl {
     //using Roles for Roles.Role;
@@ -19,6 +20,10 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl
     //   - Details of times when warranty was availed (should be in backend ig???)
     //     - Backend should be checking blockchain as source of truth so ig so
     // - public functions to external, when done
+
+    // - function to buy warranty time and warranty times
+    // - Only customer can set applied to true and false, company can only detect custom event
+    // - Company can however call a function that will trigger events to let user know that something was done (source of truth)
     
     // apparently solidity works better with 256, using smaller datatype leads to more cost
     // uint256 public constant LOYALTY_TOKEN = 0; //ID of loyalty token, only token that is non-fungible
@@ -37,6 +42,7 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl
         bool voidWhenSold;
         bool soulBound;
         bool applied;  //don't know if needed or not
+        address minter;
     }
 
 
@@ -52,17 +58,22 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl
         return super.supportsInterface(interfaceId);
     }
 
-    function safeTransferFrom(address from, address to, uint256 id, uint256 amount, bytes memory data) public virtual override(ERC1155) {
-        // is there any internal before mint function?
-        require(!(Warranties[id].soulBound), "Cannot be transferred since this is a soulBound NFT!");
-        require(!(Warranties[id].applied), "Cannot transfer when applied for warranty");
-        if(Warranties[id].voidWhenSold){
-            Warranties[id].times = -1;  // no longer valid
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data) internal virtual override(ERC1155) {
+        console.log(operator, from, to);
+        if(from != 0x0000000000000000000000000000000000000000){
+            for(uint i = 0; i < ids.length ; i++) {
+                require(!(Warranties[ids[i]].soulBound), "Cannot be transferred since this is a soulBound NFT!");
+                require(!(Warranties[ids[i]].applied), "Cannot transfer when applied for warranty");
+                if(Warranties[ids[i]].voidWhenSold){
+                    Warranties[ids[i]].times = -1;  // no longer valid
+                }
+                if(Warranties[ids[i]].decay){
+                    require(block.timestamp < Warranties[ids[i]].timestamp, "Your NFT has decayed!");
+                }
+            }
         }
-        if(Warranties[id].decay){
-            require(block.timestamp < Warranties[id].timestamp, "Your NFT has decayed!");
-        }
-        super.safeTransferFrom(from, to, id, amount, data);
+        console.log("_beforeTokenTransfer (kekigible) called");
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
     }
 
     /////////////////////////////////Modifiers////////////////////////////////
@@ -91,7 +102,7 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl
         uint timestamp,
         bool voidWhenSold,
         bool soulBound
-        ) payable public onlyMinters {
+        ) payable public onlyMinters returns(uint256) {
             require(!(hasRole(BANNED, account)), "Address is banned!");
             _tokenIDs.increment();
             uint256 id = _tokenIDs.current();
@@ -101,10 +112,12 @@ contract Kekigible is ERC1155("localhost:8000/product/{id}.json"), AccessControl
                 timestamp,
                 voidWhenSold,
                 soulBound,
-                false  //no one would apply for warranty service during buy
+                false,  //no one would apply for warranty service during buy
+                msg.sender
             );
 
             _mint(account, id, 1, data);    
+            return id;
     }
 
     // function reward(address account, uint256 amount, bytes memory data) public {

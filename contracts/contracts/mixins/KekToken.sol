d0c78065fc6582592debb18a6fae227391e0218f
rik61072@gmail.com <rik61072@gmail.com>
2022-07-24 01:07:49 +0530

    divided new code into factory, kiosk and mixin

---
+++ contracts/contracts/mixins/KekToken.sol
@@ -0,0 +1,53 @@
+//SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.9;
+
+import "./KeKAccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+
+/**
+ * @dev KekToken
+ * This is a token that will be used as loyalty tokens in the kekigible network.
+ * It has Context (eventual overriding by gas station network) and some other
+ * OpenZeppelin goodies
+ */
+contract KekToken is KekAccessControl, ERC20Capped, ERC20Burnable, ERC20Pausable {
+
+    constructor(uint256 cap) public ERC20("KekToken", "KEK") ERC20Capped(cap) {
+    }
+
+    ///////////////////////////////Pause Related stuff/////////////////////////////////
+    /**
+     * @dev Pauses all token transfers.
+     */
+    function pause() onlyPauser public virtual {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses all token transfers.
+     */
+    function unpause() onlyPauser public virtual {
+        _unpause();
+    }
+
+    //////////////////////Mint Related Stuff////////////////////////////////////////////
+
+    /**
+     * @dev function mints tokens to the `to` address
+     */
+    function mint(address to, uint256 amount) onlyMinter public virtual {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Pausable) {
+        super._beforeTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Capped) {
+        super._mint(account, amount);
+    }
+
+}

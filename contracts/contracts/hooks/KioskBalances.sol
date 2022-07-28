06152f29160362139d50e8c3e329d9d7b914c1bf
rik61072@gmail.com <rik61072@gmail.com>
2022-07-28 11:55:18 +0530

    kioskbalances

--- contracts/contracts/hooks/KioskBalances.sol
+++ contracts/contracts/hooks/KioskBalances.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.9;
 
 import "../mixins/KekAccessControl.sol";
 
+/**
+ * @dev This contract is an independent contract that will be used to keep track
+ * of balances of different kiosks. Will be very easy for API related stuff and
+ * for future expansion of the protocol.
+ * Note: should be deployed independently
+ */
 contract KioskBalances is KekAccessControl {
     
 }
